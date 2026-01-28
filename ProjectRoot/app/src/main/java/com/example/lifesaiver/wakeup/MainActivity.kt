717fea08917a466b4c20beeccde8bcd14dfca598
전희수 <acesk123@gmail.com>
2026-01-28 11:34:40 +0900

    feat: sensor wakeup impl

---
+++ ProjectRoot/app/src/main/java/com/example/lifesaiver/wakeup/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.wakeup
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 이제 SensorService가 정상이라면 빨간 줄이 안 뜰 겁니다.
+        val serviceIntent = Intent(this, SensorService::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+    }
+}
