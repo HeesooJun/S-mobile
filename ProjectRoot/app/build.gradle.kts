d5ba1fc162fc93b9f94c80b1b0a4d831d2e96a31
김명진 <usbdhffo112@naver.com>
2026-01-28 10:52:09 +0900

    feat: room database 코드 추가

--- ProjectRoot/app/build.gradle.kts
+++ ProjectRoot/app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.detekt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -67,6 +68,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
