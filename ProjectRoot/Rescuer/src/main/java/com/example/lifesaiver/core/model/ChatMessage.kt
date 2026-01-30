7659c571c8977ba115e71e5a84461fc99c889418
전희수 <acesk123@gmail.com>
2026-01-30 21:07:30 +0900

    feat: move protocol core to shared

--- ProjectRoot/Rescuer/src/main/java/com/example/lifesaiver/core/model/ChatMessage.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lifesaiver.core.model
-
-data class ChatMessage(
-    val text: String,
-    val isMine: Boolean,
-    val timestamp: Long = System.currentTimeMillis(),
-    val path: String? = null
-)
--- ProjectRoot/settings.gradle.kts
+++ ProjectRoot/settings.gradle.kts
@@ -20,8 +20,9 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Lifesaiver"
-include(":lifesaiver", ":rescuer")
+include(":lifesaiver", ":rescuer", ":shared")
 
 project(":lifesaiver").projectDir = file("Lifesaiver")
 project(":rescuer").projectDir = file("Rescuer")
+project(":shared").projectDir = file("shared")
  
